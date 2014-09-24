fdccb61e391b95f1ecac773cf19518d7f93d7411
michaelhedejakobsen <mhj2652@email.vccs.edu>
2014-09-23 20:15:57 -0400

    Table

---
+++ TableAssignment2.java
@@ -0,0 +1,35 @@
+
+public class TableAssignment2 {
+
+	public static void main(String[] args) {
+		//Return int
+	    int no1 = (1%2);
+	    System.out.println(no1);
+	    
+	    //Return double
+	    double no2 = (1.0 / 2);
+	    System.out.println(no2);
+	    
+	    //Return int
+	    int no3 = (5 + (7 / 2));
+	    System.out.println(no3);
+
+	    //Return String
+	    String no4 = ("Beat" + ' ' + "Army");
+	    System.out.println(no4);
+
+	    //Return int
+	    int no5 = (6 + (13 / 5) - (35 % 3));
+	    System.out.println(no5);
+
+	    //Return double
+	    double no6 = (3.5 * (5 / 4));
+	    System.out.println(no6);
+
+	    //Return double
+	    double no7 = ((3.5 * 5) / 4);
+	    System.out.println(no7);
+
+	}
+
+}
